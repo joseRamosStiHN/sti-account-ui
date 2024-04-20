90fc2db20615cb19994459237cf6da19d6efe87c
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2024-04-19 19:51:35 -0600

    implementing module accounting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,31 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbComponent, BreadcrumbItemDirective } from 'xng-breadcrumb';
 
+import { locale, loadMessages } from 'devextreme/localization';
+import esMessages from 'devextreme/localization/messages/es.json';
+import { AccountingModule } from './modules/accounting/accounting.module';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent,CommonModule,RouterModule,
-     BreadcrumbComponent, BreadcrumbItemDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CommonModule,
+    RouterModule,
+    BreadcrumbComponent,
+    BreadcrumbItemDirective,
+    AccountingModule,
+  ],
 })
 export class AppComponent {
-  title = 'sti-proyect';
+  title = 'Módulo contabilidad';
+
+  constructor() {
+    loadMessages(esMessages);
+    locale('es-HN');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,23 @@
 import { Routes } from '@angular/router';
-import { AccountingDashboardComponent } from './admin/accounting/accounting-dashboard/accounting-dashboard.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 export const routes: Routes = [
-
-
-
-  { path: '', component: DashboardComponent,data: {
-    breadcrumb: {
-      label: 'Home',
-      info: { myData: { icon: 'home', iconType: 'material' } }
-    }
-  } },
-  
   {
-    path: 'accounting',
-    loadChildren: () => import('./admin/admin.routes').then(m => m.ADMIN_ROUTES),
+    path: '',
+    component: DashboardComponent,
+    data: {
+      breadcrumb: {
+        label: 'Home',
+        info: { myData: { icon: 'home', iconType: 'material' } },
+      },
+    },
   },
 
-
+  {
+    path: 'accounting',
+    loadChildren: () =>
+      import('./modules/accounting/accounting.module').then(
+        (m) => m.AccountingModule
+      ),
+  },
 ];
