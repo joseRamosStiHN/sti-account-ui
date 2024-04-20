570735cb0ce285ef21f90b348b67a5e12c7b03df
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2024-04-19 19:48:57 -0600

    modified header to render dynamic menus

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,34 +1,53 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { NavigationService, NavStiLink } from '../navigation.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule, RouterModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
+    RouterModule,
+  ],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
+  nav: NavStiLink[] = [];
 
-  public nombreUsuario: string = "Josue Rodriguez";
+  // nombreUsuario: string = 'Josue Rodriguez';
 
-  tieneFotoPerfil: boolean = false;
+  // tieneFotoPerfil: boolean = false;
 
-  iniciales?: string;
+  // iniciales?: string;
 
+  constructor(private navigate: NavigationService) {}
 
   ngOnInit(): void {
-    
-    // Calcular iniciales
+    /*     // Calcular iniciales
     const nombreDividido = this.nombreUsuario.split(' ');
     const primerNombre = nombreDividido[0][0];
     const apellido = nombreDividido[nombreDividido.length - 1];
     const primerApellido = apellido ? apellido[0] : ''; // Verificar si hay apellido
-    this.iniciales = (primerNombre + primerApellido).toUpperCase();
+    this.iniciales = (primerNombre + primerApellido).toUpperCase(); */
+
+    this.subscription.add(
+      this.navigate.navLinks$.subscribe((links) => {
+        this.nav = links;
+      })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
