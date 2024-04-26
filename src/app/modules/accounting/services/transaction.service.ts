1988028236d9b34f6d34fdb895f4bdfa2cf96a83
Lcaceressti97 <lcaceres@stiglobals.com>
2024-04-26 17:12:02 -0600

    Creacion de servicio de transacciones

---
+++ src/app/modules/accounting/services/transaction.service.ts
@@ -0,0 +1,66 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionService {
+  private apiURL = 'http://localhost:8080';
+
+  /*------------------------------------------
+  --------------------------------------------
+  Http Header Options
+  --------------------------------------------
+  --------------------------------------------*/
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  constructor(private httpClient: HttpClient) {}
+
+  /**
+   * Method that brings a list with all the transactions
+   *
+   * @return response()
+   */
+  getAll(): Observable<any> {
+    return this.httpClient
+      .get(this.apiURL + '/api/v1/transaction')
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  /**
+   * Method to create a transaction
+   *
+   * @return response()
+   */
+  createTransaction(data: any): Observable<any> {
+    return this.httpClient
+      .post(
+        this.apiURL + '/api/v1/transaction',
+        JSON.stringify(data),
+        this.httpOptions
+      )
+
+      .pipe(catchError(this.errorHandler));
+  }
+
+  /**
+   * Error Handler Method
+   *
+   * @return response()
+   */
+  errorHandler(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+}
