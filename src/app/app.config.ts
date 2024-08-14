0e7d89e328e6d61aec39c1c67f6f9e2dbd3f7180
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2024-08-13 23:00:49 -0600

    modified configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,22 @@
-import { ApplicationConfig, ErrorHandler } from '@angular/core';
+import {
+  ApplicationConfig,
+  ErrorHandler,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 import { GlobalErrorHandler } from './shared/handlers/errors/GlobalErrorHandler';
+import { BrowserModule } from '@angular/platform-browser';
+import { showControlDirective } from 'src/app/shared/directives/showControlDirective';
+import { GlobalHttpErrorHandler } from 'src/app/shared/handlers/httpInterceptors/GlobalHttpErrorHandler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +25,12 @@ export const appConfig: ApplicationConfig = {
     provideAnimations(),
     provideHttpClient(withFetch()),
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: GlobalHttpErrorHandler,
+      multi: true,
+    },
+    importProvidersFrom(BrowserModule),
+    importProvidersFrom(showControlDirective),
   ],
 };
