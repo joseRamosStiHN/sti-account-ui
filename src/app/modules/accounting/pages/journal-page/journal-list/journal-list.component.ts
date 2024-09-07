9dc73441dece901a4072f870332add7747294f88
Josue Rodriguez <josue.rodriguez@baas.hn>
2024-09-07 12:41:18 -0600

    add Configuracion de diarios

--- src/app/modules/accounting/pages/journal-page/journal-list/journal-list.component.ts
+++ src/app/modules/accounting/pages/journal-page/journal-list/journal-list.component.ts
@@ -1,7 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { JournalTypes } from 'src/app/modules/accounting/models/JournalModel';
 import { JournalService } from 'src/app/modules/accounting/services/journal.service';
+import { confirm } from 'devextreme/ui/dialog';
+import { ToastType } from 'devextreme/ui/toast';
+import { typeToast } from 'src/app/modules/accounting/models/models';
 
 @Component({
   selector: 'app-journal-list',
@@ -9,21 +13,77 @@ import { JournalService } from 'src/app/modules/accounting/services/journal.serv
   styleUrl: './journal-list.component.css'
 })
 export class JournalListComponent {
+
+
+
+  messageToast: string = '';
+  showToast: boolean = false;
+  toastType: ToastType = typeToast.Info;
+
+
+
   journalList$: Observable<any> | undefined;
 
   private readonly router = inject(Router);
-  private readonly accountService = inject(JournalService);
+  private readonly journalService = inject(JournalService);
 
 
   ngOnInit(): void {
-    this.journalList$ = this.accountService.getAllAccountingJournal();
+    this.journalList$ = this.journalService.getAllAccountingJournal().pipe(
+      map(data =>  {
+       return data.map((journal)=>{
+        if (journal.accountType == JournalTypes.Bancos) {
+          journal.defaultAccountName = journal.bankAccountName;
+          return journal;
+        }
+        if (journal.accountType == JournalTypes.Efectivo) {
+          journal.defaultAccountName = journal.cashAccountName;
+          return journal;
+        }
+        return journal;
+        })
+      })
+    );
   }
 
   onEditJournal(e: any) {
-    this.router.navigate(['/accounting/configuration/update-journal', e.id]);
+
+    this.router.navigate(['/accounting/configuration/journal/update-journal', e.id]);
   }
 
   goToNewJournal = () => {
-    this.router.navigate(['/accounting/journal/new-journal']);
+    this.router.navigate(['/accounting/configuration/journal/new-journal']);
   };
+
+  async updateJournal(data:any){
+    let dialogo = await confirm(`¿Está seguro de que desea activar configuracion?`, 'Advertencia');
+    if (!dialogo) {
+      data.data.status = false;      
+      return;
+    }
+
+    const journalRequest = data.data;
+
+    if (journalRequest.accountType == JournalTypes.Bancos) {
+      journalRequest.defaultAccountName = null;
+   
+    } else if (journalRequest.accountType == JournalTypes.Efectivo) {
+      journalRequest.defaultAccountName = null;
+    }
+
+    this.journalService.updateJournal(journalRequest.id, journalRequest).subscribe({
+      next: (data) => {
+        this.toastType = typeToast.Success;
+        this.messageToast = 'Registros Actualizado correctamente';
+        this.showToast = true;
+      },
+      error: (err) => {
+        console.error('Error creating transaction:', err);
+        this.toastType = typeToast.Error;
+        this.messageToast = 'Error al actualizar Diario';
+        this.showToast = true;
+      },
+    });
+ 
+   }
 }
--- src/app/modules/accounting/pages/journal-page/journal/journal-page.component.ts
+++ src/app/modules/accounting/pages/journal-page/journal/journal-page.component.ts
@@ -1,7 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastType } from 'devextreme/ui/toast';
 import { type } from 'os';
-import { JournalModel } from 'src/app/modules/accounting/models/JournalModel';
+import { of, switchMap, tap } from 'rxjs';
+import { AccountAPIResponse, AccountTypeResponse } from 'src/app/modules/accounting/models/APIModels';
+import { JournalModel, JournalTypes } from 'src/app/modules/accounting/models/JournalModel';
+import { typeToast } from 'src/app/modules/accounting/models/models';
+import { AccountService } from 'src/app/modules/accounting/services/account.service';
+import { JournalService } from 'src/app/modules/accounting/services/journal.service';
+
+
+
 
 @Component({
   selector: 'app-journal-page',
@@ -10,32 +20,181 @@ import { JournalModel } from 'src/app/modules/accounting/models/JournalModel';
 })
 export class JournalPageComponent {
 
+
+  showToast: boolean = false;
+  toastType: ToastType = typeToast.Info;
+  messageToast: string = '';
+
+  JournalTypes = JournalTypes;
   journalForm: JournalModel;
+  journalTypes: AccountTypeResponse[] = [];
+  accounts: AccountAPIResponse[] = [];
+  accountsFilter: AccountAPIResponse[] = [];
+  private readonly accountService = inject(AccountService);
+  private readonly activeRouter = inject(ActivatedRoute);
+  private readonly journalService = inject(JournalService);
+  private readonly router = inject(Router);
+
+  id: string | null = null;
+
+  constructor() {
+    this.journalForm = {
+      diaryName: "",
+      accountType: 0,
+      defaultIncomeAccount: null,
+      defaultExpenseAccount: null,
+      cashAccount: null,
+      lossAccount: null,
+      transitAccount: null,
+      profitAccount: null,
+      bankAccount: null,
+      accountNumber: null,
+      defaultAccount: null,
+      code: ''
+    };
+
+  }
+
+  ngOnInit(): void {
+
+    this.accountService.getAllAccount().pipe(
+      tap(data => {
+ 
+        this.accounts = data.filter(account => account.supportEntry === true);
+      }),
+      switchMap(() => this.activeRouter.paramMap), 
+      switchMap(params => {
+        this.id = params.get('id');
+        const findId = Number(this.id);
+    
+        if (findId) {
+        
+          return this.journalService.getJournalById(findId);
+        } else {
+        
+          return of(null);
+        }
+      })
+    ).subscribe(journal => {
+ 
+      if (journal) {
+        this.loadAccountsOnEdit(journal);
+      }
+    });
+
+    this.journalService.getAllAccountType().subscribe({
+      next: (data) => {
+        this.journalTypes = data;
+      }
+    });
 
 
-  journalTypes:string[]= ["Ventas","Compras","Efectivo","Bancos","Varios"]
-
-
-  constructor(){
-    this.journalForm={
-      id: 0,
-      name: "",
-      type: "",
-      code: "",
-      accountCash:"",
-      accountPredeterminate: "",
-      transitAccount:"",
-      lossAccount:"",
-      profitAccount:"",
-      bankAccount:"",
-      accountNumber: "",
-      status: true
-  };
-}
+  }
 
   onSubmit(e: NgForm) {
-    
+
+    if (this.id) {
+      this.journalService.updateJournal(Number(this.id), this.journalForm).subscribe({
+        next: (data) => {
+          this.toastType = typeToast.Success;
+          this.messageToast = 'Registros actualizados exitosamente';
+          this.showToast = true;
+          setTimeout(() => {
+            this.goBack();
+          }, 1000);
+        },
+        error: (err) => {
+          console.error('Error creating transaction:', err);
+          this.toastType = typeToast.Error;
+          this.messageToast = 'Error al actualizar Diario';
+          this.showToast = true;
+        },
+      });
+
+      return
+    }
+
+    if (e.valid) {
+
+      this.journalService.createJoural(this.journalForm).subscribe({
+        next: (data) => {
+          this.toastType = typeToast.Success;
+          this.messageToast = 'Registros insertados exitosamente';
+          this.showToast = true;
+          this.router.navigate(['/accounting/configuration/journal-list']);
+        },
+        error: (err) => {
+          console.error('Error creating transaction:', err);
+          this.toastType = typeToast.Error;
+          this.messageToast = 'Error al crear la transacción';
+          this.showToast = true;
+        },
+      });
+
+    }
+
   }
+
+  onChangeType(e: any) {
+    if (e.target.value == JournalTypes.Ventas) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Ventas);
+      return;
+    }
+
+    if (e.target.value == JournalTypes.Compras) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Compras);
+      return;
+    }
+
+    if (e.target.value == JournalTypes.Varios) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Varios);
+      return;
+    }
+
+    if (e.target.value == JournalTypes.Bancos) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Bancos);
+      return;
+    }
+    if (e.target.value == JournalTypes.Efectivo) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Efectivo);
+      return;
+    }
+
+
+  }
+   loadAccountsOnEdit(journal: JournalModel) {
+
+    this.journalForm = journal;
+
+    if (journal.accountType == JournalTypes.Ventas) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Ventas);
+      return;
+    }
+
+    if (journal.accountType == JournalTypes.Compras) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Compras);
+      return;
+    }
+
+    if (journal.accountType == JournalTypes.Varios) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Varios);
+      return;
+    }
+
+    if (journal.accountType == JournalTypes.Bancos) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Bancos);
+      return;
+    }
+    if (journal.accountType == JournalTypes.Efectivo) {
+      this.accountsFilter = this.accounts.filter(account => account.accountType == JournalTypes.Efectivo);
+      return;
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/accounting/configuration/journal-list']);
+  }
+
 
 
 }
