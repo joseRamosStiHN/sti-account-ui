2ad11b86fdca149439718d34a68fa88e078a8f99
Josue Rodriguez <josue.rodriguez@baas.hn>
2024-09-13 17:57:16 -0600

    cambios en los asientos contables

--- src/app/modules/accounting/pages/accounting-page/journal-entries/journal-entries.component.ts
+++ src/app/modules/accounting/pages/accounting-page/journal-entries/journal-entries.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Component, inject, OnInit } from '@angular/core';
+import { BehaviorSubject, catchError, map, Observable, of } from 'rxjs';
+import { JournalTypes } from 'src/app/modules/accounting/models/JournalModel';
+import { BillingListClient } from 'src/app/modules/accounting/models/models';
+import { TransactionService } from 'src/app/modules/accounting/services/transaction.service';
 
 //TODO: esto cambien lo esto es solo para maquetar
 interface LocalData {
@@ -20,28 +23,85 @@ interface LocalData {
 })
 export class JournalEntriesComponent implements OnInit {
   data$: Observable<LocalData[] | null> = new BehaviorSubject(null);
+  dataSource$: Observable<BillingListClient[]> | undefined;
+  private readonly transService = inject(TransactionService);
+
+
+  dataTable:LocalData[]= [];
 
   ngOnInit(): void {
     const d = this.generateRandomLocalData();
     this.data$ = new BehaviorSubject(d);
+    
+    this.transService.getAll().subscribe({
+      next: (data) => {
+        this.dataTable = this.fillDataSource(data);
+      },
+    });
+    
+  }
+
+
+  fillDataSource(data: any[]): LocalData[] {
+
+     return data.map((item: any) => {
+      const totalDetail = item.transactionDetails.find((element: any) => {
+        if (item.documentType === JournalTypes.Ventas && element.entryType === "Credito") {
+          return true; 
+        } else if (item.documentType === JournalTypes.Compras && element.entryType === "Debito") {
+          return true;
+        }
+        return false;
+      });
+
+     
+      
+
+      return {
+        id: item.id,
+        date:item.date,
+        referenceNumber: item.reference,
+        reference:item.documentType == JournalTypes.Ventas || item.documentType == JournalTypes.Compras
+        ? "" : item.description,
+        journalEntry: item.documentType == JournalTypes.Ventas ? "Ventas":"Compras",
+        total:totalDetail.amount,
+        status: item.status.toUpperCase() === 'DRAFT' ? 'Borrador' : 'Confirmado',
+      } as LocalData;
+      
+      
+    })
+
+    
   }
 
   generateRandomLocalData(): LocalData[] {
-    const randomData: LocalData[] = [];
+    let randomData: LocalData[] = [];
     const types = ['Factura de clientes', 'Factura de proveedor', 'Bancos'];
     const states = ['Publicado', 'Borrador'];
-    for (let i = 0; i < 5; i++) {
-      randomData.push({
-        id: i + 1,
-        referenceNumber: Math.random().toString(36).substring(7),
-        reference: `Ref-${i + 1}`,
-        journalEntry: types[Math.floor(Math.random() * types.length)],
-        journalEntryId: Math.floor(Math.random() * 100),
-        total: parseFloat((Math.random() * 1000).toFixed(2)),
-        status: states[Math.floor(Math.random() * states.length)],
-        date: new Date(Date.now() - Math.floor(Math.random() * 10000000000)),
-      });
-    }
+    // for (let i = 0; i < 5; i++) {
+    //   randomData.push({
+    //     id: i + 1,
+    //     referenceNumber: Math.random().toString(36).substring(7),
+    //     reference: `Ref-${i + 1}`,
+    //     journalEntry: types[Math.floor(Math.random() * types.length)],
+    //     journalEntryId: Math.floor(Math.random() * 100),
+    //     total: parseFloat((Math.random() * 1000).toFixed(2)),
+    //     status: states[Math.floor(Math.random() * states.length)],
+    //     date: new Date(Date.now() - Math.floor(Math.random() * 10000000000)),
+    //   });
+    // }
+    this.transService.getAll().subscribe({
+      next: (data) => {
+        randomData = data.map((item :any)=> ({
+            id: item.id,
+            referenceNumber: item.entryNumber,
+            reference: item.description,
+            journalEntry:item.documentType,
+            total:100,
+            status:"Borrado"
+          } as LocalData));
+      },
+    });
     return randomData;
   }
 }
--- src/app/modules/accounting/pages/accounting-page/journal-items/journal-items.component.ts
+++ src/app/modules/accounting/pages/accounting-page/journal-items/journal-items.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { JournalTypes } from 'src/app/modules/accounting/models/JournalModel';
+import { TransactionService } from 'src/app/modules/accounting/services/transaction.service';
 
 interface LocalJournalItem {
   id: number;
@@ -7,6 +9,7 @@ interface LocalJournalItem {
   journalEntry?: string; // este es Número de Factura proveedor | cliente | banco | Varios etc...
   defaultAccount?: string;
   reference?: string;
+  description?:string;
   debit?: number;
   credit?: number;
   balance?: number;
@@ -19,10 +22,49 @@ interface LocalJournalItem {
 })
 export class JournalItemsComponent implements OnInit {
   data$: Observable<LocalJournalItem[] | null> = new BehaviorSubject(null);
+  private readonly transService = inject(TransactionService);
+
+  dataTable: LocalJournalItem[] = [];
 
   ngOnInit(): void {
     const d = this.generateRandomLocalJournalItems();
     this.data$ = new BehaviorSubject(d);
+
+
+
+    this.transService.getAll().subscribe({
+      next: (data) => {
+        this.dataTable = this.fillDataSource(data);
+      },
+    });
+
+  }
+
+
+  fillDataSource(data: any[]): LocalJournalItem[] {
+    const result: LocalJournalItem[] = [];
+    
+    data.forEach((item: any) => {
+      item.transactionDetails.forEach((transaction: any) => {
+        const debito = transaction.entryType === 'Credito' ? 0 : transaction.amount;
+        const credito = transaction.entryType === 'Debito' ? 0 : transaction.amount;
+        const localJournalItem: LocalJournalItem = {
+          id: item.id,
+          reference: item.reference,
+          date: item.creationDate,
+          journalEntry:item.diaryName,
+          defaultAccount: `${transaction.accountName} ${transaction.accountCode}`,
+          description: item.description,
+          debit: debito,
+          credit: credito,
+          balance: Number((debito - credito).toFixed(2)),
+        };
+        result.push(localJournalItem);
+      });
+    });
+
+    return result;
+
   }
 
   generateRandomLocalJournalItems(): LocalJournalItem[] {
