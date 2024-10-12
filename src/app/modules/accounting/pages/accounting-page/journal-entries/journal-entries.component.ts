a58fe2023d9caa466e4d13bcfc2aaa49d4228427
Josue Rodriguez <josue.rodriguez@baas.hn>
2024-10-12 08:46:12 -0600

    agregando notas de debito y credito

--- src/app/modules/accounting/pages/accounting-page/journal-entries/journal-entries.component.ts
+++ src/app/modules/accounting/pages/accounting-page/journal-entries/journal-entries.component.ts
@@ -45,25 +45,49 @@ export class JournalEntriesComponent implements OnInit {
   fillDataSource(data: any): LocalData[] {
     const result: LocalData[] = [];
 
-    const mapTransactionToLocalData = (item: any, isAdjustment: boolean = false): LocalData => {
+    const mapTransactionToLocalData = (item: any, isAdjustment: boolean = false, 
+      isCreditNotes:boolean=false, isDebitNotes:boolean=false): LocalData => {
       const totalDetail = isAdjustment
         ? item.adjustmentDetails.reduce((acc: number, detail: any) => {
-          const credit = detail.credit || 0;     
+          const type = detail.typicalBalance;
+          const credit = type == "C" && detail.shortEntryType == "C"? detail.amount : type =="D"
+          && detail.shortEntryType == "D"
+          ? detail.amount :0;    
             return acc + credit;
         }, 0)
+        : isCreditNotes ? 
+        item.detailNote.reduce((acc: number, detail: any) => {
+          const type = detail.typicalBalance;
+          const credit = type == "C" && detail.shortEntryType == "C"? detail.amount : type =="D"
+          && detail.shortEntryType == "D"
+          ? detail.amount :0;    
+            return acc + credit;
+        }, 0)
+        
+        : isDebitNotes? item.detailNote.reduce((acc: number, detail: any) => {          
+          const type = detail.typicalBalance;
+        
+          const credit = type == "C" && detail.shortEntryType == "C"? detail.amount : type =="D"
+          && detail.shortEntryType == "D"
+          ? detail.amount :0;   
+            return acc + credit;
+        }, 0) 
         : item.transactionDetails.find((element: any) => {
           return (item.documentType === JournalTypes.Ventas && element.entryType === "Credito") ||
             (item.documentType === JournalTypes.Compras && element.entryType === "Debito");
         });
 
-      const totalAmount = isAdjustment ? totalDetail : totalDetail ? totalDetail.amount : 0;
+      const totalAmount = isAdjustment ? totalDetail : 
+      isDebitNotes ? totalDetail :
+      isCreditNotes ? totalDetail:
+      totalDetail ? totalDetail.amount : 0;
 
       return {
         id: item.id,
         date: isAdjustment ? item.creationDate : item.date,
-        referenceNumber: isAdjustment ? item.invoiceNo : item.reference,
+        referenceNumber: isAdjustment || isDebitNotes || isCreditNotes ? item.invoiceNo : item.reference,
         reference: (item.documentType === JournalTypes.Ventas || item.documentType === JournalTypes.Compras)
-          ? "" : isAdjustment ? item.reference : item.description,
+          ? "" : isAdjustment || isDebitNotes || isCreditNotes? item.reference : item.description ,
         journalEntry: item.diaryName,
         total:  totalAmount,
         status: item.status.toUpperCase() === 'DRAFT' ? 'Borrador' : 'Confirmado',
@@ -78,6 +102,15 @@ export class JournalEntriesComponent implements OnInit {
 
     data.adjustments.forEach((item: any) => {
       result.push(mapTransactionToLocalData(item, true));
+    });
+
+
+    data.debitNotes.forEach((item: any) => {
+      result.push(mapTransactionToLocalData(item, false,false,true));
+    });
+
+    data.creditNotes.forEach((item: any) => {
+      result.push(mapTransactionToLocalData(item, false,true));
     });
 
 
