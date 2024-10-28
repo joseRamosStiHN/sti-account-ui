d43ff46632ca85b6bdb150af1732bc01986855a7
Josue Rodriguez <josue.rodriguez@baas.hn>
2024-10-28 17:20:41 -0600

    modificacion en inicializacion de balance

--- src/app/modules/accounting/pages/balance-accounts/balance-accounts/balance-accounts.component.ts
+++ src/app/modules/accounting/pages/balance-accounts/balance-accounts/balance-accounts.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastType } from 'devextreme/ui/toast';
 import { AccountModel, BalancesModel } from 'src/app/modules/accounting/models/AccountModel';
@@ -16,6 +16,13 @@ export class State {
   Name!: string;
 }
 
+interface accountSummary{
+  name:string
+  code:string
+  typeAccount:string,
+  fatherAccount:string
+}
+
 @Component({
   selector: 'app-balance-accounts',
   templateUrl: './balance-accounts.component.html',
@@ -23,15 +30,22 @@ export class State {
 })
 export class BalanceAccountsComponent {
 
-
+  @Input('id') id?: string;
   messageToast: string = '';
   showToast: boolean = false;
   toastType: ToastType = typeToast.Info;
 
+  sumaryAccount:accountSummary={
+    code:"",
+    name:"",
+    fatherAccount:"",
+    typeAccount:""
+  }
+
 
 
   accountPrefix: string = '';
-  id: string | null = null;
+
   balanceResponse!: BalanceResponse[];
   activeAdd!: boolean;
   activeEdit!: boolean;
@@ -57,8 +71,8 @@ export class BalanceAccountsComponent {
   }
 
   ngOnInit(): void {
-    this.activeAdd =false;
-    this.activeEdit =false;
+    this.activeAdd = false;
+    this.activeEdit = false;
 
     this.periodService.getStatusPeriod().subscribe({
       next: (status) => {
@@ -70,16 +84,15 @@ export class BalanceAccountsComponent {
     }
     );
 
-    this.activeRouter.paramMap.subscribe((params) => {
-      this.id = params.get('id');
-      const findId = Number(this.id);
-      if (findId) {
-        this.accountService.findAccountById(findId).subscribe({
-          next: (data) => this.fillForm(data),
-          error: (err) => console.error('error: we got ', err),
-        });
-      }
-    });
+
+    const findId = Number(this.id);
+    if (findId) {
+      this.accountService.findAccountById(findId).subscribe({
+        next: (data) => this.fillForm(data),
+        error: (err) => console.error('error: we got ', err),
+      });
+    }
+
 
   }
 
@@ -88,19 +101,21 @@ export class BalanceAccountsComponent {
 
     let balanceSave: BalancesModel = {
       createAtDate: new Date(),
-      isActual: true,
+      isCurrent: true,
       initialBalance: e.data.initialBalance,
-      typicalBalance: e.data.typicalBalance,
+      typicalBalance: e.data.typicalBalance == "Debito" ? "D" : "C",
       accountId: Number(this.id)
     }
 
     this.balanceService.saveBalance(balanceSave).subscribe({
       next: (data) => {
-        
-        this.activeAdd= false
+
+        this.activeAdd = false
         this.toastType = typeToast.Success;
         this.messageToast = 'Registros inicialisados exitosamente';
         this.showToast = true;
+        this.activeEdit = true;
+        this.redirectTo();
 
       },
       error: (err) => {
@@ -108,22 +123,17 @@ export class BalanceAccountsComponent {
         this.toastType = typeToast.Error;
         this.messageToast = 'Error al inicializar saldos';
         this.showToast = true;
+        this.redirectTo();
       },
     });
 
-    // setTimeout(() => {
-      
-    // this.ngOnInit();
- 
-    // }, 1000);
 
   }
 
 
-  update(e: any){
-    
-    let balance: BalancesModel = {
+  update(e: any) {
 
+    let balance: BalancesModel = {
       createAtDate: e.data.createAtDate,
       isActual: true,
       initialBalance: e.data.initialBalance,
@@ -131,30 +141,23 @@ export class BalanceAccountsComponent {
       accountId: Number(this.id)
     }
 
-    
-
     this.balanceService.updateBalance(Number(e.data.id), balance).subscribe({
       next: (data) => {
         this.toastType = typeToast.Success;
         this.messageToast = 'Registros actualizados exitosamente';
         this.showToast = true;
-        setTimeout(() => {
-      
-        }, 1000);
+        this.redirectTo();
       },
       error: (err) => {
         console.error('Error creating transaction:', err);
         this.toastType = typeToast.Error;
         this.messageToast = 'Error al crear el periodo';
         this.showToast = true;
+        this.redirectTo();
       },
     });
 
-    // setTimeout(() => {
-    //   this.ngOnInit();
-    // }, 1000);
 
-   
   }
 
   private fillForm(data: AccountAPIResponse): void {
@@ -162,6 +165,8 @@ export class BalanceAccountsComponent {
     this.accountForm.category = data.categoryId;
     this.accountForm.code = data.accountCode;
     this.accountForm.description = data.name;
+ 
+
     if (data.typicallyBalance.toUpperCase() === 'CREDITO') {
       this.accountForm.typicalBalance = 'C';
     } else {
@@ -172,7 +177,14 @@ export class BalanceAccountsComponent {
     this.accountPrefix = data.parentCode ?? '';
     this.accountForm.isActive =
       data.status.toUpperCase() === 'ACTIVA' ? true : false;
-    this.accountForm.balances = data.balances;
+
+
+    this.accountForm.balances = data.balances.map((balance) => {
+      return {
+        ...balance,
+        typicalBalance: balance.typicalBalance === "D" ? "Debito" : "Credito"
+      };
+    });
 
     if (this.accountForm.balances.length < 1) {
       this.activeAdd = true;
@@ -182,9 +194,16 @@ export class BalanceAccountsComponent {
       this.activeEdit = true;
     }
 
+    this.sumaryAccount=  {
+      name: data.name,
+      code: data.accountCode,
+      fatherAccount: data.parentName?? '',
+      typeAccount: data.categoryName,
+    };
+
   }
 
-  async react(){
+  async react() {
 
     let dialogo = await confirm(`¿No existe un periodo Activo desea activarlo?`, 'Advertencia');
     if (!dialogo) {
@@ -193,5 +212,11 @@ export class BalanceAccountsComponent {
     }
 
     this.router.navigate(['/accounting/configuration/period']);
+  }
+
+  redirectTo() {
+    setTimeout(() => {
+      this.router.navigate(['/accounting/configuration/balance/accounts']);
+    }, 3000);
   }
 }
