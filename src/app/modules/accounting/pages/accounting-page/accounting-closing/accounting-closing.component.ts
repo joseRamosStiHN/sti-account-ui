5c877a58cdbfa70985abb81efa26b332607d249f
Josue Rodriguez <josue.rodriguez@baas.hn>
2024-12-17 21:36:40 -0600

    subiendo cambio de visualizacion de fechas de nuevo periodo

--- src/app/modules/accounting/pages/accounting-page/accounting-closing/accounting-closing.component.ts
+++ src/app/modules/accounting/pages/accounting-page/accounting-closing/accounting-closing.component.ts
@@ -28,20 +28,20 @@ export class AccountingClosingComponent {
     { value: 'WEEKLY', text: 'Semanal' }
   ];
 
-  
+
 
   private readonly periodService = inject(PeriodService);
   private readonly router = inject(Router);
-  infoClosing?:PeriodClosing;
-  periodsClosing:ClosingPeriodsAll[]=[];
-  nextPeriod:NextPeridModel = {
+  infoClosing?: PeriodClosing;
+  periodsClosing: ClosingPeriodsAll[] = [];
+  nextPeriod: NextPeridModel = {
     closureType: "",
     startPeriod: "",
     endPeriod: "",
     daysPeriod: 0
   };
 
-  constructor(){
+  constructor() {
     this.periodService.getInfoClosingPeriod().subscribe({
       next: (info) => {
         this.infoClosing = info;
@@ -58,7 +58,7 @@ export class AccountingClosingComponent {
         }, 3000);
       }
     });
-    
+
     this.periodService.getAllClosing().subscribe({
       next: (info) => {
         this.periodsClosing = info;
@@ -74,8 +74,8 @@ export class AccountingClosingComponent {
 
     this.periodService.getNextPeriod().subscribe({
       next: (data) => {
-        data.startPeriod = data.startPeriod.substring(0,10);
-        data.endPeriod = data.endPeriod.substring(0,10)
+        data.startPeriod = data.startPeriod.substring(0, 10);
+        data.endPeriod = data.endPeriod.substring(0, 10)
         this.nextPeriod = data;
       },
       error: (err) => {
@@ -86,44 +86,44 @@ export class AccountingClosingComponent {
         this.showToast = true;
       }
     });
-    
+
   }
 
-  openModal(){
+  openModal() {
     this.isPopupVisible = true;
   }
 
 
-  closing() {   
-    
+  closing() {
+
     console.log(this.nextPeriod);
-    
+
     let dialogo = confirm(
       `¿Está seguro de que desea realizar esta acción?`,
       'Advertencia'
     );
 
     dialogo.then(async (d) => {
-    
-        this.periodService.closingPeriod(this.nextPeriod.closureType).subscribe({
-          next: () => {
-            this.toastType = typeToast.Success;
-            this.messageToast = 'Periodo Cerrdado Correctamente';
-            this.showToast = true;
 
-            setTimeout(() => {
-              this.router.navigate(['/accounting/configuration/period']); 
-            }, 3000);
-          },
-          error: (err) => {
-            console.log(err);
-            
-            this.toastType = typeToast.Error;
-            this.messageToast = 'Error al intentar cerrar periodo';
-            this.showToast = true;
-          },
-        });
-      }
+      this.periodService.closingPeriod(this.nextPeriod.closureType).subscribe({
+        next: () => {
+          this.toastType = typeToast.Success;
+          this.messageToast = 'Periodo Cerrdado Correctamente';
+          this.showToast = true;
+
+          setTimeout(() => {
+            this.router.navigate(['/accounting/configuration/period']);
+          }, 3000);
+        },
+        error: (err) => {
+          console.log(err);
+
+          this.toastType = typeToast.Error;
+          this.messageToast = 'Error al intentar cerrar periodo';
+          this.showToast = true;
+        },
+      });
+    }
     );
   }
 
@@ -137,91 +137,101 @@ export class AccountingClosingComponent {
   }
 
   onChange() {
-      this.calculateEndDate(this.nextPeriod)
+    console.log(this.nextPeriod);
+
+    this.calculateEndDate(this.nextPeriod)
   }
 
   calculateEndDate(object: any) {
+
+
     const { closureType, startPeriod } = object;
-    const startDate = new Date(startPeriod);
+
+    const startDate = new Date(`${startPeriod}T00:00:00-06:00`);
+
     const startMonth = startDate.getMonth();
     const startYear = startDate.getFullYear();
-  
+
     let endMonth = startMonth;
     let endYear = startYear;
-  
-    // Determinar el mes y año final basado en el closureType
+
+
     switch (closureType) {
       case 'Mensual':
-        endMonth += 1;
+        endMonth += 0;
         break;
       case 'Trimestral':
-        endMonth += 3;
+        endMonth += 2;
         break;
       case 'Semestral':
-        endMonth += 6;
+        endMonth += 5;
         break;
     }
-  
+
+    if (closureType === 'Trimestral' && endMonth == 12) {
+      endMonth = 11;
+      endYear = startYear;
+
+    }
+
+    if (closureType === 'Semestral' && endMonth > 6) {
+
+      endMonth = 11;
+      endYear = startYear;
+
+    }
 
     if (endMonth > 11) {
       endYear += Math.floor(endMonth / 12);
       endMonth %= 12;
     }
-  
 
-    if (closureType === 'Trimestral' || closureType === 'Semestral') {
-      if (endYear > startYear || endMonth > 11) {
-        endMonth = 11; 
-        endYear = startYear; 
-      }
-    }
-  
 
     const lastDay = this.getLastDayOfMonth(endYear, endMonth);
-  
+
 
     const endDate = new Date(endYear, endMonth, lastDay);
-  
+
 
     const oneDay = 24 * 60 * 60 * 1000;
     const diffDays = Math.round(Math.abs((endDate.getTime() - startDate.getTime()) / oneDay));
-  
+
 
     object.endPeriod = endDate.toISOString().substring(0, 10);
     object.startPeriod = startDate.toISOString().substring(0, 10);
     object.daysPeriod = diffDays;
-  
+
     return object;
   }
-  
+
   getLastDayOfMonth(year: number, month: number): number {
     return new Date(year, month + 1, 0).getDate();
   }
 
-  dowloadPdf(periodsClosing:any){
+  dowloadPdf(periodsClosing: any) {
 
-   const base64Data = periodsClosing.closureReportPdf;
-   if (!base64Data) {
-     console.error("No se encontró el contenido Base64 del PDF.");
-     return;
-   }
+    const base64Data = periodsClosing.closureReportPdf;
+    if (!base64Data) {
+      console.error("No se encontró el contenido Base64 del PDF.");
+      return;
+    }
 
-   const byteCharacters = atob(base64Data); 
-   const byteNumbers = Array.from(byteCharacters, char => char.charCodeAt(0));
-   const byteArray = new Uint8Array(byteNumbers);
-   const blob = new Blob([byteArray], { type: 'application/pdf' });
- 
-   const link = document.createElement('a');
-   const url = URL.createObjectURL(blob);
-   link.href = url;
-   link.download = periodsClosing.startPeriod + '-'+periodsClosing.endPeriod; 
-   link.click();
+    const byteCharacters = atob(base64Data);
+    const byteNumbers = Array.from(byteCharacters, char => char.charCodeAt(0));
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: 'application/pdf' });
 
-   URL.revokeObjectURL(url);
-      
+    const link = document.createElement('a');
+    const url = URL.createObjectURL(blob);
+    link.href = url;
+    link.download = periodsClosing.startPeriod + '-' + periodsClosing.endPeriod;
+    link.click();
+
+    URL.revokeObjectURL(url);
+
   }
- 
-  
+
+
 }
 
 
